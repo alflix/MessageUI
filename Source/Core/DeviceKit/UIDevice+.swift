14058f8e6d728bd7bc38ac9f6b004dce85da2550
alflix <jieyuanz24@gmail.com>
2019-09-18 23:43:17 +0800

    add: SYSTEM_VERSION check

--- Source/Core/DeviceKit/UIDevice+.swift
+++ Source/Core/DeviceKit/UIDevice+.swift
@@ -68,3 +68,23 @@ public extension UIDevice {
         return addresses.first
     }
 }
+
+func SYSTEM_VERSION_EQUAL_TO(version: String) -> Bool {
+    return UIDevice.current.systemVersion.compare(version, options: .numeric) == .orderedSame
+}
+
+func SYSTEM_VERSION_GREATER_THAN(version: String) -> Bool {
+    return UIDevice.current.systemVersion.compare(version, options: .numeric) == .orderedDescending
+}
+
+func SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(version: String) -> Bool {
+    return UIDevice.current.systemVersion.compare(version, options: .numeric) != .orderedAscending
+}
+
+func SYSTEM_VERSION_LESS_THAN(version: String) -> Bool {
+    return UIDevice.current.systemVersion.compare(version, options: .numeric) == .orderedAscending
+}
+
+func SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(version: String) -> Bool {
+    return UIDevice.current.systemVersion.compare(version, options: .numeric) != .orderedDescending
+}
